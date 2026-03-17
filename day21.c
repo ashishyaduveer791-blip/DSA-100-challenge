c75751489b3ef801def7e2df29cb24c8a433a4e1
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-03-17 23:59:40 +0530

    Add files via upload

---
+++ day21.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+
+        for (int j = i; j < n; j++)
+        {
+            sum = sum + arr[j];
+
+            if (sum == 0)
+            {
+                count++;
+            }
+        }
+    }
+
+    printf("Number of subarrays with sum = 0 is: %d", count);
+
+    return 0;
+}

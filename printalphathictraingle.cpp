f649beca913086dd119e7a7d09ad0a87119dcc91
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-04-14 23:56:12 +0530

    Add files via upload

---
+++ printalphathictraingle.cpp
@@ -0,0 +1,56 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     char ch = 'A';
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j <= i; j++)
+//         {
+//             cout << ch;
+//             /* code */
+//         }
+//         cout << endl;
+//         ch++;
+
+//         /* code */
+//     }
+
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n = 6;
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         for (int j = 1; j < i + 1; j++)
+//         {
+//             cout << j;
+//             /* code */
+//         }
+//         cout << endl;
+
+//         /* code */
+//     }
+//     return 0;
+// }
+#include <iostream>
+ussing namespace std;
+int main()
+{
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j > 0; j--)
+        {
+            cout << j;
+            /* code */
+        }
+        cout << endl;
+
+        /* code */
+    }
+    return 0;
+}

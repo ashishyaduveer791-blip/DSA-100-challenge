2947247a5c0b5c1a4f12843d820413508c462fa7
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-09-14 22:41:31 +0530

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,59 @@
+// stl (std.templete did
+//  not include vector header file)
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main()
+// {
+//     vector<int> vec = {1, 2, 3};
+//     cout << vec[0] << endl;
+//     return 0;
+// }
+
+// Anoher way to do
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main()
+// {
+//     vector<int> vec(5, 0);
+//     cout << vec[0] << endl;
+//     cout << vec[1] << endl;
+//     cout << vec[2] << endl;
+//     return 0;
+// }
+
+// One more method
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main()
+// {
+//     vector<char> vec = {'a', 'b', 'c', 'd', 'e'};
+//     cout << "size = " << vec.size() << endl;
+//     for (char val : vec)
+//     {
+//         cout << val << endl;
+//     }
+//     return 0;
+// }
+
+
+// push Back
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector <int> vec
+    cout << "size=" << vec.size() << endl;
+    vec.push_back(25);
+    vec.push back(35);
+    vec.push_back(45);
+    cout << "after push back size =" << vec.size()<<endl;
+    for(int val : vec){
+        
+    }
+}
+
+
